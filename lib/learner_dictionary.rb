1d0450e0beb286952dc2f5ccd47c96aedb1287f4
Wouter Valentin <wouter@sustabit.nl>
2016-10-24 13:05:32 +0200

    Added a source language to translate method.

--- lib/learner_dictionary.rb
+++ lib/learner_dictionary.rb
@@ -1,8 +1,12 @@
 class LearnerDictionary
-  def translate(word)
+  def translate(word, source_lang)
+  	# source_lang is three letter string from ISO 639-2/T
+  	return 'otya' if word == 'tea' and source_lang == 'jpn'
+  	return 'solcha' if word == 'tea' and source_lang == 'bod'
     return 'inu' if word == 'dog'
+    return 'khyi' if word == 'dog'
     return 'neko' if word == 'cat'
-    return 'sayonara' if word == 'hello'
-    return 'otya' if word == 'tea'
+    return 'sayonara' if word == 'hello' and source_lang == 'jpn'
+    return 'tashidelek' if word == 'hello' and source_lang == 'bod'
   end
 end
--- spec/learner_dictionary_spec.rb
+++ spec/learner_dictionary_spec.rb
@@ -5,15 +5,21 @@ describe LearnerDictionary do
   subject(:learner_dictionary) { described_class.new }
 
   it 'should provide a translation of dog in Japanese' do
-    expect(learner_dictionary.translate('dog')).to eq 'inu'
+    expect(learner_dictionary.translate('dog','jpn')).to eq 'inu'
   end
   it 'should provide a translation of cat in Japanese' do
-    expect(learner_dictionary.translate('cat')).to eq 'neko'
+    expect(learner_dictionary.translate('cat','jpn')).to eq 'neko'
   end
   it 'should provide a translation of hello in Japanese' do
-    expect(learner_dictionary.translate('hello')).to eq 'sayonara'
+    expect(learner_dictionary.translate('hello','jpn')).to eq 'sayonara'
   end
   it 'should provide a translation of tea in Japanese' do
-  	expect(learner_dictionary.translate('tea')).to eq 'otya'
+  	expect(learner_dictionary.translate('tea','jpn')).to eq 'otya'
+  end
+  it 'should provide a translation of tea in Tibetan' do
+  	expect(learner_dictionary.translate('tea','bod')).to eq 'solcha'
+  end
+  it 'should provide a translation of hello in Tibetan' do
+  	expect(learner_dictionary.translate('hello','bod')).to eq 'tashidelek'
   end
 end
